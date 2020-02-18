bc6d51af2fedc66d467448864a3120baaaccff4f
tourman <8002615@gmail.com>
2020-02-18 12:47:25 +0200

    wip: TasksView styles

--- components/Todo/Tasks/View.js
+++ components/Todo/Tasks/View.js
@@ -4,45 +4,7 @@ import { Modal, Button, View, FlatList, StyleSheet, Text, TextInput, Platform } 
 import { observer, useDoc, useQuery } from 'startupjs'
 
 import Task from '../Task';
-
-const isAndroid = Platform.OS === 'android'
-const viewPadding = 10
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    padding: viewPadding,
-    paddingTop: 20
-  },
-  list: {
-    width: '100%'
-  },
-  listItem: {
-    paddingTop: 2,
-    paddingBottom: 2,
-    fontSize: 18
-  },
-  hr: {
-    height: 1,
-    backgroundColor: 'gray'
-  },
-  listItemCont: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  textInput: {
-    height: 40,
-    paddingRight: 10,
-    paddingLeft: 10,
-    borderColor: 'gray',
-    borderWidth: isAndroid ? 0 : 1,
-    width: '100%'
-  }
-})
+import './View.styl';
 
 const renderItem = ({ item: { id } }) => pug`
   Task(
@@ -52,11 +14,8 @@ const renderItem = ({ item: { id } }) => pug`
 
 export default function TasksView ({ text, filter, tasks, onAdd, onSearch, onChangeText, onChangeFilter }) {
   return pug`
-    View(
-      style=[styles.container, { paddingBottom: viewPadding }]
-    )
-      TextInput(
-        style=styles.textInput
+    View.container
+      TextInput.input(
         onChangeText=onChangeFilter
         onSubmitEditing=onSearch
         value=filter
@@ -64,13 +23,11 @@ export default function TasksView ({ text, filter, tasks, onAdd, onSearch, onCha
         returnKeyType='done'
         returnKeyLabel='done'
       )
-      FlatList(
-        style=styles.list
+      FlatList.list(
         data=tasks
         renderItem=renderItem
       )
-      TextInput(
-        style=styles.textInput
+      TextInput.input(
         onChangeText=onChangeText
         onSubmitEditing=onAdd
         value=text
